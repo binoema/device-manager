3a3c2eb05657045032689cca3b0ce16c63f7f525
Marc Binöder <marc.binoeder@gmx.de>
2024-02-27 22:34:13 +0100

    adjusted interface for file handling

--- DeviceManagerApi/Interface/ICrudService.cs
+++ DeviceManagerApi/Interface/ICrudService.cs
@@ -5,7 +5,7 @@
         Task<List<T>> GetAllAsync();
 
         public Task<List<T>> Remove(string id);
-        public Task<List<T>> AddAsync(List<T> devices);
+        public Task<List<T>> AddAsync(IFormFile devicesFile);
 
 
 
