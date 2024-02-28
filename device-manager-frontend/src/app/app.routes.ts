c1fb71eae5116e954cc8abc5cf959dad06442007
Marc Binöder <marc.binoeder@gmx.de>
2024-02-28 20:36:24 +0100

    added device detail view

--- device-manager-frontend/src/app/app.routes.ts
+++ device-manager-frontend/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { DevicesComponent } from './devices/devices.component';
+import { DeviceDetailViewComponent } from './devices/device-card/device-detail-view/device-detail-view.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/device', pathMatch: 'full' },
+  {
+    path: 'device',
+    component: DevicesComponent,
+  },
+  {
+    path: 'device/:id',
+    component: DeviceDetailViewComponent,
+    data: { device: {} }
+  }
+];
---
+++ device-manager-frontend/src/app/devices/device-card/device-detail-view/device-detail-view.component.ts
@@ -0,0 +1,29 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { DeviceStore } from '../../device.store';
+import { Router } from '@angular/router';
+
+@Component({
+    selector: 'app-device-detail-view',
+    standalone: true,
+    imports: [
+        CommonModule,
+    ],
+    templateUrl: './device-detail-view.component.html',
+    styleUrl: './device-detail-view.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DeviceDetailViewComponent {
+
+  router = inject(Router);
+  store = inject(DeviceStore);
+  device = this.store.selectedDevice;
+
+  get temp() {
+    return `${this.device()?.tempMin} - ${this.device()?.tempMax} `
+  }
+
+  routeBackToOverview() {
+    this.router.navigate(["device"]);
+  }
+ }
